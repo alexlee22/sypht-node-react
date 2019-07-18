75e5d0f398ede181c41849e0eca3d38b47a82bca
Alexander Lee <10814755+alexlee22@users.noreply.github.com>
2019-07-18 21:47:30 +1000

    Updated Node.JS server with comments and sorting

--- server/index.js
+++ server/index.js
@@ -1,14 +1,34 @@
+/*
+- - - - - - - - - -
+DEPENDENCIES
+- - - - - - - - - -
+*/
+
 const fs = require('fs');
 const path = require('path');
-
-const express = require('express')
-const cors = require('cors')
-const app = express()
-app.use(cors())
-const port = 3000
-
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(cors());
 const request = require('request');
-const multer  = require('multer')
+const multer  = require('multer'); // for uploading file
+const mongoose = require('mongoose');
+
+
+/*
+- - - - - - - - - -
+VARIABLES
+- - - - - - - - - -
+*/
+
+// Sypht private keys <Don't Upload>
+const clientID = "<CLIENTID>";
+const clientSecret = "<CLIENTSECRET>";
+const port = 3000;
+const mongoDBAddress = 'mongodb://localhost:27017/sypht';
+
+
+// Prep Multer
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, 'uploads')
@@ -20,34 +40,13 @@ var storage = multer.diskStorage({
 var upload = multer({ storage: storage }).single('file')
 
 
-const clientID = "<CLIENTID>";
-const clientSecret = "<CLIENTSECRET>";
-
-
-
-//mongodb
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/sypht', {useNewUrlParser: true});
+// Set up MongoDB
+mongoose.connect(mongoDBAddress, {useNewUrlParser: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-  // we're connected!
+    console.log("Connected to MongoDB!")
 });
-
-/*
-var invoiceSchema = new mongoose.Schema({
-    fileId: String,
-    timestamp: String,
-    total: String,
-    amountPaid: String,
-    tax: String,
-    gst: String,
-    amountDue: String,
-    subTotal: String,
-    supplierABN: String
-});
-*/
-
 
 var invoiceSchema = new mongoose.Schema({
     fileId: String,
@@ -67,7 +66,14 @@ var invoiceSchema = new mongoose.Schema({
 const Invoice = mongoose.model('invoice', invoiceSchema);
 
 
+/*
+- - - - - - - - - -
+FUNCTIONS
+- - - - - - - - - -
+*/
 
+// Authenticate to the Sypht server
+// Returns `access_token` required to access the server
 function authenticateSypht() {
     let options = {
         url:'https://login.sypht.com/oauth/token',
@@ -91,13 +97,14 @@ function authenticateSypht() {
     })
 }
 
-//Upload file to Sypht server
-function uploadSypht(access_token, filePath) {
+
+// Upload file to Sypht server
+// Requires authenticated and file path on server
+function uploadToSypht(access_token, filePath) {
     let options = {
         url:'https://api.sypht.com/fileupload',
         formData: {
             fileToUpload: fs.createReadStream(path.join(__dirname, filePath)),
-            //fieldSets:JSON.stringify(['sypht.invoice','sypht.document'])
             fieldSets:JSON.stringify(['sypht.invoice'])
         },
         headers:{
@@ -105,7 +112,7 @@ function uploadSypht(access_token, filePath) {
         },
         json:true
     }
-      
+
     return new Promise((resolve, reject) => {
         request.post(options, (error, response, body) => {
             resolve(body);
@@ -113,7 +120,9 @@ function uploadSypht(access_token, filePath) {
     })
 }
 
-//Download Sypht data
+
+// Download data of a file from Sypht
+// Uses a fileId and access_token from auth
 function downloadInvoice(access_token, fileId) {
     let options = {
         url:'https://api.sypht.com/result/final/' + fileId,
@@ -126,7 +135,8 @@ function downloadInvoice(access_token, fileId) {
     return new Promise((resolve, reject) => {
         request.get(options, (error, response, body) => {
             var returnDict = {}
-            for (let i = 0; i < body.results.fields.length; i++) { //array.reduce instead?
+            //Get value if not null
+            for (let i = 0; i < body.results.fields.length; i++) { //note: array.reduce instead?
                 let splitName = body.results.fields[i].name.split('.')
                 if (body.results.fields[i].value !== null) {
                     returnDict[splitName[1]] = Number(body.results.fields[i].value)
@@ -139,11 +149,9 @@ function downloadInvoice(access_token, fileId) {
     })
 }
 
-//- - - - - - - - - 
-
-
-
-function uploadFile(req, res){
+// Upload file to server at the location
+// Uses multer
+function uploadFileToServer(req, res){
     return new Promise((resolve, reject) => {
         upload(req, res, (err) => {
             resolve(req.file)
@@ -151,21 +159,46 @@ function uploadFile(req, res){
     })
 }
 
+// Search and receive the fileId document from the MongoDB
+// Return: dict
+function searchDB(fileId){
+    return new Promise((resolve, reject) => {
+        Invoice.find({ 'fileId': fileId }, function (err, invoices) {
+            if (err) return console.error(err);
+            resolve(invoices[0])
+        })
+    })
+}
+
+// TODO: Update document in MongoDB
+function updateDocument (fileId, data) {
+
+}
 
 
-//POST: Image to server then Sypht
+/*
+- - - - - - - - - -
+VARIABLES
+- - - - - - - - - -
+*/
+
+// POST: Image to server then Sypht
+// Return: fileId and Name
 async function uploadImage(req, res) {
+
     var auth = await authenticateSypht()
-    var file = await uploadFile(req, res)
-    var uploadResult = await uploadSypht(auth, file.path)
+    var file = await uploadFileToServer(req, res)
+    var uploadResult = await uploadToSypht(auth, file.path)
     
-    //Insert into MongoDB
+    // Insert into MongoDB
     var makeSave = new Invoice({
         fileId: uploadResult.fileId,
         status: uploadResult.status,
         uploadedAt: Date.parse(uploadResult.uploadedAt),
         data: {}
     });
+
+    // Return after save
     makeSave.save(function (err) {
         if (err) return handleError(err);
         res.send(uploadResult)
@@ -173,13 +206,15 @@ async function uploadImage(req, res) {
 }
 
 
-//GET: List of incoices
+// GET: List of invoices from MongoDB
+// Return: list dict of a compiled name and fileId
 async function getInvoicesList(req, res){
     var getInvoices = await Invoice.find(function (err, invoices) {
         if (err) return console.error(err);
         return invoices;
     })    
     
+    // Make a list dict for name and fileId
     var invoiceNames = getInvoices.map((inv) => {
         return ({
             'name': inv.uploadedAt + ' - ' + inv.fileId,
@@ -192,29 +227,18 @@ async function getInvoicesList(req, res){
     })
 }
 
-function searchDB(fileId){
-    return new Promise((resolve, reject) => {
-        Invoice.find({ 'fileId': fileId }, function (err, invoices) {
-            if (err) return console.error(err);
-            resolve(invoices[0])
-        })
-    })
-}
 
-function updateDocument (fileId, data) {
-
-}
-
-
-//GET: One invoice
+// GET: specific invoice data
+// Return: data from MongoDB and/or Sypht
 async function getSpecificInvoices(req, res){
     if (req.query.fileId != undefined){
         var getInvoiceData = await searchDB(req.query.fileId);
+        
+        // If have not gotten data from Sypht, get and update
         if (getInvoiceData.status === "RECEIVED"){
             var auth = await authenticateSypht()
             var result = await downloadInvoice(auth, req.query.fileId);
 
-            //replace new data
             getInvoiceData.status = "FINALISED";
             getInvoiceData.data = result
             //UpdateDB: updateDocument()
@@ -232,10 +256,19 @@ async function getSpecificInvoices(req, res){
 }
 
 
+/*
+- - - - - - - - - -
+ROUTES
+- - - - - - - - - -
+*/
 
 app.get('/', (req, res) => res.send('Hello World!'));
+
+// Returns list of data list with `date` and `fileId`
 app.get('/invoiceslist', (req, res) => getInvoicesList(req, res));
+// Returns data of `fileId`
 app.get('/invoicedata', (req, res) => getSpecificInvoices(req, res));
+// Uploads image to server and Sypht
 app.post('/uploadImage', (req, res) => uploadImage(req, res));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
